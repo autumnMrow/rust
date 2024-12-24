54b4af7567d14ee686f5d4d072b511d6866b539e
autumnMrow <abigayl.nutini@gmail.com>
2024-12-24 04:23:51 -0800

    deleted target folder

--- guessing_game/target/debug/deps/cfg_if-8217498f8667ada9.d
+++
@@ -1,7 +0,0 @@
-/mnt/d/code/guessing_game/target/debug/deps/libcfg_if-8217498f8667ada9.rmeta: /home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/mnt/d/code/guessing_game/target/debug/deps/libcfg_if-8217498f8667ada9.rlib: /home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/mnt/d/code/guessing_game/target/debug/deps/cfg_if-8217498f8667ada9.d: /home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs:
--- guessing_game/target/debug/deps/rand_chacha-e9c8238c95d53752.d
+++
@@ -1,9 +0,0 @@
-/mnt/d/code/guessing_game/target/debug/deps/librand_chacha-e9c8238c95d53752.rmeta: /home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
-
-/mnt/d/code/guessing_game/target/debug/deps/librand_chacha-e9c8238c95d53752.rlib: /home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
-
-/mnt/d/code/guessing_game/target/debug/deps/rand_chacha-e9c8238c95d53752.d: /home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
-
-/home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs:
-/home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs:
-/home/autumn/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs:
